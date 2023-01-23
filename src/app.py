fc008fe4f641bee0a5d5d4c8b2ab8c11ee6f6e8a
DmitreyKazin98 <dmitrey.kazin@bynet.co.il>
2023-01-23 16:20:08 +0000

    working version

--- src/app.py
+++ src/app.py
@@ -41,11 +41,8 @@ def installer():
     if request.method == "POST":
         package = request.form.get("package-select")
         package_file_key = list_packages.get_packages()[package]
-        try:
-            s3_controller.download_file(package_file_key)
-            return render_template("success.html", package=package, package_file_key=package_file_key)
-        except:
-            return 
+        s3_controller.download_file(package_file_key)
+        return render_template("success.html", package=package, package_file_key=package_file_key)
 
     if request.method == "GET":
         packages = list(list_packages.get_packages().keys())
