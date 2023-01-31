8fceccb2d0040ba81a0d9dd587bf69b06c8efafc
Dmitrey  Kazin <dmitrey.kazin@bynet.co.il>
2023-01-31 13:07:28 +0200

    fix

--- src/app.py
+++ src/app.py
@@ -72,8 +72,16 @@ def details():
             # Send a POST request to Bynet CRM
 	    url = "https://bynetdev.service-now.com/api/bdml/aws_api/new_lic"
 	    headers = {"Content-Type": "application/json"}
-            record = {"timestamp": timestamp, "company_name": company_name, "first_name": first_name, "last_name": last_name, "email": email, "phone_number": phone_number, "package": package}
-            response = requests.post(url, headers=headers, json=record)
+        record = {
+            "timestamp": timestamp,
+            "company_name": company_name,
+            "first_name": first_name, 
+            "last_name": last_name,
+            "email": email,
+            "phone_number": phone_number,
+            "package": package
+        }
+        response = requests.post(url, headers=headers, json=record)
 
             # Notify about successful change
             flash("Your details saved successfuly!", category="success")
