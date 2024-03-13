588ba0a73abd4964df7fc269ae82839868c6d209
Oz Pozner <OzP@bynet.co.il>
2024-03-14 01:52:46 +0200

    submitted on  1:52:46.57

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd /opt
+sudo cd /opt
 sudo git clone https://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@github.com/OzPozner-bynet/bynet_installer.git
-cd bynet_installer
-sudo chmod a+x ./which_cloud.sh
-./which_cloud.sh
-sudo python src/aws_info.py
-sudo chmod a+x ./install_start.sh
-sudo cp ./bynet_installer.service /etc/systemd/system/
+sudo cd /opt/bynet_installer
+sudo chmod a+x /opt/bynet_installer/which_cloud.sh
+sudo /opt/bynet_installer/which_cloud/which_cloud.sh
+sudo python /opt/bynet_installer/src/aws_info.py
+sudo chmod a+x /opt/bynet_installer/src/install_start.sh
+sudo cp /opt/bynet_installer/src/bynet_installer.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable bynet_installer.service
 sudo systemctl start bynet_installer.service
