fe2271a5747a55df6f2a5fde21ff14789791ce4f
Oz Pozner <OzP@bynet.co.il>
2024-03-13 21:16:32 +0200

    bug fix

--- src/aws_info.py
+++ src/aws_info.py
@@ -1,13 +1,15 @@
-import os, requests, boto3, dotenv
+import os, requests, boto3, dotenv, json
 # get data from aws meta-deta
 token = requests.put("http://169.254.169.254/latest/api/token", headers={"X-aws-ec2-metadata-token-ttl-seconds": "21600"}).text
 ec2_instance_id = requests.get("http://169.254.169.254/latest/meta-data/instance-id", headers={"X-aws-ec2-metadata-token": token}).text
-aws_account_id = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document", headers={"X-aws-ec2-metadata-token": token}).text
+inJSONtext= requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document", headers={"X-aws-ec2-metadata-token": token}).text
+inJSON = json.loads(inJSONtext)
 print("adding/updating in .env")
 print("ec2_instance_id="+ec2_instance_id)
-print("aws_account_id="+aws_account_id )
-account_id = aws_account_id["accountId"]
-mpProductCodes = aws_account_id["marketplaceProductCodes"]
+print("inJson="+inJSON )
+account_id = inJSON["accountId"]
+print("account_id="+account_id )
+mpProductCodes = inJSON["marketplaceProductCodes"]
 from dotenv import load_dotenv, dotenv_values
 # Load environment variables from the `.env` file
 load_dotenv()
