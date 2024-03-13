19a01b5ca1784ba59d6a93ecc8202acd47d4a922
Oz Pozner <OzP@bynet.co.il>
2024-03-14 01:54:46 +0200

    submitted on  1:54:46.00

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo cd /opt
-sudo git clone https://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@github.com/OzPozner-bynet/bynet_installer.git
-sudo cd /opt/bynet_installer
+cd /opt
+git clone https://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@github.com/OzPozner-bynet/bynet_installer.git
+cd /opt/bynet_installer
 sudo chmod a+x /opt/bynet_installer/which_cloud.sh
 sudo /opt/bynet_installer/which_cloud/which_cloud.sh
 sudo python /opt/bynet_installer/src/aws_info.py
