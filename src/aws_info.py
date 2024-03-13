24c759048b30abcc69029aa393441220b522ff37
Oz Pozner <OzP@bynet.co.il>
2024-03-13 21:27:43 +0200

    bug fix

--- src/aws_info.py
+++ src/aws_info.py
@@ -6,7 +6,6 @@ inJSONtext= requests.get("http://169.254.169.254/latest/dynamic/instance-identit
 inJSON = json.loads(inJSONtext)
 print("adding/updating in .env")
 print("ec2_instance_id="+ec2_instance_id)
-print("inJson="+inJSON )
 account_id = inJSON["accountId"]
 print("account_id="+account_id )
 mpProductCodes = inJSON["marketplaceProductCodes"]
@@ -14,17 +13,17 @@ from dotenv import load_dotenv, dotenv_values
 # Load environment variables from the `.env` file
 load_dotenv()
 # Check if 'aws_account_id' key exists
-if "aws_account_id" not in dotenv_values():
+if "AWS_ACCOUNT_ID" not in dotenv_values():
   # Key not found, add it with a placeholder value (replace with your desired default)
   with open(".env", "a") as env_file:
-    env_file.write("aws_account_id="+account_id+"\n")
-if "ec2_instance_id" not in dotenv_values():
+    env_file.write("AWS_ACCOUNT_ID="+account_id+"\n")
+if "EC2_INSTANCE_ID" not in dotenv_values():
   # Key not found, add it with a placeholder value (replace with your desired default)
   with open(".env", "a") as env_file:
-    env_file.write("ec2_instance_id="+ec2_instance_id+"\n")
+    env_file.write("EC2_INSTANCE_ID="+ec2_instance_id+"\n")
 
 # Now you can access environment variables using os.getenv()
 aws_account_id = os.getenv("aws_account_id")
 print(f"AWS Account ID (from .env): {aws_account_id}")
-ec2_instance_id = os.getenv("ec2_instance_id")
+ec2_instance_id = os.getenv("EC2_INSTANCE_ID")
 print(f"AWS Instance ID (from .env): {ec2_instance_id}")
