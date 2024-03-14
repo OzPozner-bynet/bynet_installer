0eafff87462f98437fd343cb5d5b0fca2775e5e5
Oz Pozner <OzP@bynet.co.il>
2024-03-14 06:28:23 +0200

    submitted on  6:28:23.76

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+sudo yum update -y
+sudo yum install python3 pip3 -y 
+virtualenv /opt/bynet_installer
+source /opt/bynet_installer/bin/activate
 cd /opt
 sudo git clone https://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@github.com/OzPozner-bynet/bynet_installer.git
 cd /opt/bynet_installer
 sudo chmod a+x /opt/bynet_installer/src/which_cloud.sh
 /opt/bynet_installer/src/which_cloud.sh
 pip3 install -r /opt/bynet_installer/src/requirements.txt
-sudo python /opt/bynet_installer/src/aws_info.py
+python /opt/bynet_installer/src/aws_info.py
 sudo chmod a+x /opt/bynet_installer/src/install_start.sh
-sudo cp /opt/bynet_installer/src/bynet_installer.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable bynet_installer.service
+#on start run script usinf rc.local
+sudo chmod +x /etc/rc.d/rc.local
+sh /opt/bynet_installer/src/install_start.sh
+#on start run script using systemd
+#sudo cp /opt/bynet_installer/src/bynet_installer.service /etc/systemd/system/
+#sudo systemctl daemon-reload
+#sudo systemctl enable bynet_installer.service
 #sudo systemctl start bynet_installer.service
